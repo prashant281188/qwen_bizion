98cb098d3a22b9ec5e8d681530cd7e7905b15de3
Prashant Garg <66334619+prashant281188@users.noreply.github.com>
2026-04-24 15:20:31 +0530

    Delete apps directory

--- apps/web/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    API_URL: process.env.API_URL || 'http://localhost:3001/api',
-  },
-};
-
-module.exports = nextConfig;
